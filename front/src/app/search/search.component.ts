82d788a0073dba091f4cc17d0c957394a478c96d
Angel R <93774378+Angelgaeta@users.noreply.github.com>
2023-08-11 18:05:28 +0200

    optimisation du code avec ajout de la gestion des champs vides

--- front/src/app/search/search.component.ts
+++ front/src/app/search/search.component.ts
@@ -11,100 +11,80 @@ import { Observable } from 'rxjs';
   styleUrls: ['./search.component.css'],
 })
 export class SearchComponent implements OnInit {
-  searchForm!: FormGroup; // Déclaration du formulaire de recherche
-  error: boolean = false;
-  test: any[] = []; // Tableau pour stocker les données JSON
+  searchForm!: FormGroup; // Formulaire de recherche
+  error: boolean = false; // Flag pour gérer les erreurs
+  test: any[] = []; // Données JSON
   searchResults: string[] = []; // Tableau pour stocker les résultats de la recherche
-  private _jsonUrl = 'assets/db.json'; // URL du fichier JSON
+  private _jsonUrl = 'assets/db.json'; // URL du fichier JSON local
 
+  // Injecter FormBuilder et HttpClient via le constructeur
   constructor(
     private formBuilder: FormBuilder,
     private http: HttpClient // Utilisation de l'API HTTP d'Angular pour les requêtes
   ) {}
 
+  // Fonction pour récupérer les données JSON
   public getJSON(): Observable<any> {
     return this.http.get(this._jsonUrl); // Récupération des données JSON via HTTP
   }
 
   /**
-   * Initialisation du composant Search lorsqu'il est chargé
+   Initialisation lors du chargement du composant  
    */
   ngOnInit(): void {
     this.initsearchForm(); // Initialisation du formulaire de recherche
     this.getJSON().subscribe((data) => {
-      this.test = data; // Chargement des données JSON
+      this.test = data; // Stockage des données JSON pour une utilisation ultérieure
     });
   }
 
   /**
-   * Initialisation du champ "lieu" et "event" dans le formulaire de recherche
+   * Initialiser le formulaire avec FormBuilder
    */
   initsearchForm(): void {
     this.searchForm = this.formBuilder.group({
-      lieu: [''], // Champ "lieu" avec validation requise
-      event: [''], // Champ "event" avec validation requise
+      lieu: [''], // Champ "lieu"
+      event: [''], // Champ "event"
     });
   }
 
   /**
-   * Gestion de la soumission du formulaire de recherche
+   * Gestion de la soumission du formulaire
    */
   onsubmitSearchForm(): void {
-    // Récupération des valeurs de recherche pour le lieu et l'événement
-    const searchLieu = this.searchForm.value.lieu; // Valeur du champ "lieu"
-    const searchEvent = this.searchForm.value.event; // Valeur du champ "event"
+    // Utilisation de la déstructuration pour extraire les valeurs des champs
+    const { lieu: searchLieu, event: searchEvent } = this.searchForm.value;
+    // const searchLieu = this.searchForm.value.lieu; // Valeur du champ "lieu"
+    // const searchEvent = this.searchForm.value.event; // Valeur du champ "event"
 
     // Vérification si les deux champs sont vides
     if (!searchLieu && !searchEvent) {
-      this.error = true; // Définir la variable d'erreur sur true
-      return; // Si les deux champs sont vides, on ne fait rien
+      this.error = true; // Afficher une erreur si c'est le cas
+      return;
     }
 
     // Réinitialiser la variable d'erreur
     this.error = false;
 
-    // Si le champ "lieu" est renseigné et que le champ "événement" est vide, affichez tous les événements de cette ville
-    if (searchLieu && !searchEvent) {
-      const eventsInCity = this.test.filter((el) => el.city === searchLieu);
-      console.log('Événements dans la ville:', eventsInCity);
-      return;
-    }
+    // Filtrer les événements selon la ville et le nom de l'événement
+    const eventsInCity = this.test.filter((el) => el.city === searchLieu);
+    const specificEvent = this.test.filter(
+      (el) => el.eventName === searchEvent
+    );
 
-    // Si le champ "lieu" est vide mais que le champ "événement" est renseigné, affichez l'événement spécifié peu importe la ville
-    if (!searchLieu && searchEvent) {
-      const specificEvent = this.test.filter(
-        (el) => el.eventName === searchEvent
-      );
-      console.log('Événements spécifiques:', specificEvent);
-      return;
-    }
-
-    // Recherche d'un événement correspondant au nom de recherche
-    let foundEvent = this.test.find((el) => el.eventName === searchEvent);
-
-    // Filtre des événements correspondant à la ville de recherche
-    let foundCity = this.test.filter((el) => el.city === searchLieu);
-
-    // Vérification si les deux champs sont non vides
-    if (searchLieu && searchEvent) {
-      // Conditions pour déterminer le résultat en fonction des recherches
-      if (foundEvent && foundCity.length) {
-        // Si un événement est trouvé ET des événements dans la ville sont trouvés
-        console.log(foundEvent);
-        alert('cas 1');
-      } else if (!foundEvent && foundCity.length) {
-        // Si aucun événement n'est trouvé, mais des événements dans la ville sont trouvés
-        console.log(foundCity);
-        alert('cas 2');
-      } else if (foundEvent && !foundCity.length) {
-        // Si un événement est trouvé, mais aucun événement dans la ville n'est trouvé
-        console.log(foundEvent);
-        alert('cas 3');
-      } else {
-        // Si ni un événement ni des événements dans la ville ne sont trouvés
-        console.log('pas de résultat');
-        alert('cas 4');
-      }
+    // Évaluation des résultats filtrés et détermination de l'action à suivre
+    if (specificEvent.length && eventsInCity.length) {
+      console.log(eventsInCity);
+      alert('cas 1');
+    } else if (!specificEvent.length && eventsInCity.length) {
+      console.log(eventsInCity);
+      alert('cas 2');
+    } else if (specificEvent.length && !eventsInCity.length) {
+      console.log(specificEvent);
+      alert('cas 3');
+    } else {
+      console.log('pas de résultat');
+      alert('cas 4');
     }
   }
 }
